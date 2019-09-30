3c977acf2d2728134c833822ba3155851ffc545c
Tragon <huebeckers@gmx.de>
2019-09-30 05:11:03 +0200

    simulation option for new visit count algorithm, manhattan distance option, variable bonuses/penalties

--- toy_1d.py
+++ toy_1d.py
@@ -36,8 +36,8 @@ Reward only for getting to the goal. Doubled reward if he got the bonus prior.
 class ToyChainSparseEnv(gym.Env):
 
     def __init__(self):
-        self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(1,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(6,), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(4,), dtype=np.float32)
         self.dt = 0.1
         self.tol = 0.01
         self.goal_state = 0.6
@@ -45,55 +45,78 @@ class ToyChainSparseEnv(gym.Env):
         self._seed()
         self._step = 0
         self.step_size = 1.0
+        self.speed_penalty_factor = 0#.01
+        self.wall_penalty_factor = 0#.01
+        self.goal_reward = 1.0
+        self.bonus_reward = 1.0
+        self.partial_reward_factor = 1.0
+        max_rwd = self.bonus_reward + self.goal_reward
+        self.reward_range = (-float(max_rwd), float(max_rwd))#spaces.Box(low=-max_rwd, high=max_rwd, shape=(1,), dtype=np.float32)
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def step(self,u):
-        self._step += 1
-        u = np.clip(u, self.action_space.low, self.action_space.high)
+        simulate = False
+        if isinstance(u, list) and len(u) > 1:
+            simulate = u[1]
+            u = u[0]
+        u = np.clip(u, self.action_space.low, self.action_space.high) * self.dt
         x = self.state[0] # player pos
         xd = self.state[1] # player velocity
         b = self.state[2] # bonus pos
-        gb = self.state[4] # got bonus?
-        xd_n = xd + u*self.dt
-        x_n = x + xd*self.dt
+        gb = self.state[3] # got bonus?
+        xd_n = xd + u[0]
+        x_n = x + xd*self.dt # already u*dt
 
         done = False
-        rwd = 0.
+        rwd = 0.0
         dist_goal = np.abs(x - self.goal_state)
         dist_bonus = np.abs(x - b)
         if dist_bonus < self.tol and np.abs(xd) < self.tol:
-            gb = 1 # set bonus collected #b = self.goal_state # place bonus where the goal is
+            gb = 1.0 # set bonus collected #b = self.goal_state # place bonus where the goal is
         if dist_goal < self.tol and np.abs(xd) < self.tol:
-            rwd = 1 # overwrite bonus reward with goal reward (because you already collected it)
+            rwd = self.goal_reward # overwrite bonus reward with goal reward (because you already collected it)
             done = True
             if gb > 0:
-                rwd *= 2
+                rwd += self.bonus_reward
         if np.abs(x_n) > 1: # collision with world border
-            rwd -= 0.1 # bad boy
-            xd_n = 0   # set velocity 0
+            rwd -= 1 * self.wall_penalty_factor # bad boy
+            xd_n = 0.0   # set velocity 0
 
         # After the max step size we give the end bonus for the distance to goal. Double if we got the bonus.
-        if self._step >= self.spec.max_episode_steps and not done:
+        if self._step >= self.spec.max_episode_steps-1 and not done:
             mult = 1.0
-            if gb > 0:
-                mult = 2.0
-            rwd += (1.0 - (dist_goal / self.start_dist)) * mult
+            part_rwd = (1.0 - (dist_goal / self.start_dist)) * self.goal_reward * self.partial_reward_factor
+            if gb > 0: #and part_rwd > 0
+                #mult = 2.0
+                rwd += self.bonus_reward
+            rwd += part_rwd * mult
             done = True
 
-        self.state[0] = x_n#np.clip(x_n, self.observation_space.low, self.observation_space.high)
-        self.state[1] = xd_n
-        self.state[4] = gb
-        self.state[5] = self.step_size * self._step
-        self.state = np.clip(self.state, self.observation_space.low, self.observation_space.high)
-        return self._get_obs(), rwd, done, {}
+        rwd -= np.abs(xd_n) * self.speed_penalty_factor # penalize speed -> go slower for exploration
+
+        new_state = self.state
+        new_state[0] = x_n#np.clip(x_n, self.observation_space.low, self.observation_space.high)
+        new_state[1] = xd_n
+        new_state[3] = gb
+        #self.state[4] = self.step_size * self._step
+        new_state = np.clip(new_state, self.observation_space.low, self.observation_space.high)
+        if simulate:
+            self._step += 1
+            self.state = new_state
+        return new_state, rwd, done, {}
 
     def reset(self):
         self._step = 0
-        self.state = np.array([0,0,0,self.goal_state,-1,0])
-        self.state[2] = self.bonus_states[np.random.randint(0,2)]
+        bonus_pos = self.bonus_states[np.random.randint(0,3)]
+        self.state = np.array([0.0, # pos
+                               0.0, # velocity
+                               bonus_pos, # bonus pos
+                               -1.0 # got bonnus?
+                               #,self.goal_state
+                               ])
         self.step_size = 1.0 / self.spec.max_episode_steps
         self.last_u = None
         self.start_dist = np.abs(self.state[0] - self.goal_state)
--- toy_2d.py
+++ toy_2d.py
@@ -1,6 +1,7 @@
 import gym
 from gym import spaces
 from gym.utils import seeding
+import copy
 import numpy as np
 
 '''
@@ -36,8 +37,8 @@ Reward only for getting to the goal. Doubled reward if he got the bonus prior.
 class ToyChainSparseEnv2D(gym.Env):
 
     def __init__(self):
-        self.action_space = spaces.Box(low=-0.1, high=0.1, shape=(2,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(10,), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(7,), dtype=np.float32)
         self.dt = 0.1
         self.tol = 0.01
         self.goal_state = [0.6, 0]
@@ -47,14 +48,26 @@ class ToyChainSparseEnv2D(gym.Env):
         self._seed()
         self._step = 0
         self.step_size = 1.0
+        self.speed_penalty_factor = 0#.01
+        self.wall_penalty_factor = 0
+        self.goal_reward = 1.0
+        self.bonus_reward = 1.0
+        self.partial_reward_factor = 1.0
+        self.start_dist = 0.8
+        self.manhattan_dist = False
+        max_rwd = self.bonus_reward + self.goal_reward
+        self.reward_range = (-float(max_rwd), float(max_rwd))#spaces.Box(low=-max_rwd, high=max_rwd, shape=(1,), dtype=np.float32)
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def step(self,u):
-        self._step += 1
-        u = np.clip(u, self.action_space.low, self.action_space.high)
+        simulate = False
+        if len(u) > 2:
+            simulate = u[2]
+            u = u[:2]
+        u = np.clip(u, self.action_space.low, self.action_space.high) * self.dt
         x = self.state[0] # player pos x
         y = self.state[1] # player pos y
         xd = self.state[2] # player velocity x
@@ -62,66 +75,90 @@ class ToyChainSparseEnv2D(gym.Env):
         bx = self.state[4] # bonus pos x
         by = self.state[5] # bonus pos y
         gb = self.state[6] # got bonus?
-        xd_n = xd + u[0]*self.dt
-        x_n = x + xd*self.dt
-        yd_n = yd + u[1]*self.dt
-        y_n = y + yd*self.dt
+        xd_n = xd + u[0]
+        x_n = x + xd*self.dt # already u*dt
+        yd_n = yd + u[1]
+        y_n = y + yd*self.dt # already u*dt
 
         done = False
         rwd = 0.0
-        dist_goal = np.abs(x - self.goal_state[0]) + np.abs(y - self.goal_state[1])
-        dist_bonus = np.abs(x - bx) + np.abs(y - by)
+        dist_goal = self.calcDistance(x,self.goal_state[0], y,self.goal_state[1])
+        dist_bonus = self.calcDistance(x,bx, y,by)
         if dist_bonus < self.tol and np.abs(xd) < self.tol and np.abs(yd) < self.tol:
-            gb = 1 # set bonus collected #b = self.goal_state # place bonus where the goal is
+            gb = 1.0 # set bonus collected #b = self.goal_state # place bonus where the goal is
         if dist_goal < self.tol and np.abs(xd) < self.tol and np.abs(yd) < self.tol:
-            rwd = 1 # overwrite bonus reward with goal reward (because you already collected it)
+            rwd = self.goal_reward # overwrite bonus reward with goal reward (because you already collected it)
             done = True
             if gb > 0:
-                rwd *= 2
-        if np.abs(x_n) > 1: # collision with world border
+                rwd += self.bonus_reward
+        if np.abs(x_n) > 1.0: # collision with world border
+            rwd -= 1 * self.wall_penalty_factor # bad boy
+            xd_n = 0   # set velocity 0
+            yd_n = 0   # set velocity 0
+            x_n  = x   # reset pos
+        if np.abs(y_n) > 1.0: # collision with imaginary street border obstacles
             rwd -= 0.1 # bad boy
             xd_n = 0   # set velocity 0
             yd_n = 0   # set velocity 0
-        if np.abs(y_n) > 1: # collision with imaginary street border obstacles
-            rwd -= 0.1 # bad boy
-            xd_n = 0   # set velocity 0
-            yd_n = 0   # set velocity 0
+            y_n  = y   # reset pos
 
         # After the max step size we give the end bonus for the distance to goal. Double if we got the bonus.
-        if self._step >= self.spec.max_episode_steps and not done:
+        if self._step >= self.spec.max_episode_steps-1 and not done:
             mult = 1.0
-            if gb > 0:
-                mult = 2.0
-            rwd += (1.0 - (dist_goal / self.start_dist)) * mult
+            part_rwd = (1.0 - (dist_goal / self.start_dist)) * self.goal_reward * self.partial_reward_factor
+            if gb > 0: #and part_rwd > 0
+                #mult = 2.0
+                rwd += self.bonus_reward
+            rwd += part_rwd * mult
             done = True
 
-        self.state[0] = x_n#np.clip(x_n, self.observation_space.low, self.observation_space.high)
-        self.state[1] = xd_n
-        self.state[2] = y_n
-        self.state[3] = yd_n
-        self.state[6] = gb
-        self.state[9] = self.step_size * self._step
-        self.state = np.clip(self.state, self.observation_space.low, self.observation_space.high)
-        return self.state, rwd, done, {}
+        rwd = rwd - (np.abs(xd_n)+np.abs(yd_n)) * self.speed_penalty_factor # penalize speed -> go slower for exploration
+
+        if not simulate:
+            self.state[0] = x_n#np.clip(x_n, self.observation_space.low, self.observation_space.high)
+            self.state[1] = y_n
+            self.state[2] = xd_n
+            self.state[3] = yd_n
+            self.state[6] = gb
+            #self.state[7] = self.step_size * self._step
+            self.state = np.clip(self.state, self.observation_space.low, self.observation_space.high)
+
+            self._step += 1
+            #print("stepers", self._elapsed_steps)
+            return self.state, rwd, done, {}
+        else:
+            simstate = copy.deepcopy(self.state)
+            simstate[0] = x_n
+            simstate[1] = y_n
+            simstate[2] = xd_n
+            simstate[3] = yd_n
+            simstate[6] = gb
+            simstate = np.clip(simstate, self.observation_space.low, self.observation_space.high)
+            return simstate, rwd, done, {}
 
     def reset(self):
         self._step = 0
         b = np.random.randint(0,3)
-        self.state = np.array([0, # player pos x
-                               0, # player pos y
-                               0, # player velocity x
-                               0, # player velocity y
+        self.state = np.array([0.0, # player pos x
+                               0.0, # player pos y
+                               0.0, # player velocity x
+                               0.0, # player velocity y
                                self.bonus_states[b][0], # bonus pos x
                                self.bonus_states[b][1], # bonus pos y
-                               -1, # got bonus?
-                               self.goal_state[0], # goal pos x
-                               self.goal_state[1], # goal pos x
-                               0 # step
+                               -1.0 # got bonus?
+                               #,self.goal_state[0] # goal pos x
+                               #,self.goal_state[1] # goal pos x
+                               #0.0 # step
                                ])
         self.step_size = 1.0 / self.spec.max_episode_steps
         self.last_u = None
-        self.start_dist = np.abs(self.state[0] - self.goal_state[0]) + np.abs(self.state[1] - self.goal_state[1])
+        self.start_dist = np.sqrt((self.state[0] - self.goal_state[0])**2 + (self.state[1] - self.goal_state[1])**2)#np.abs(self.state[0] - self.goal_state[0]) + np.abs(self.state[1] - self.goal_state[1])
         return self._get_obs()
 
     def _get_obs(self):
         return self.state
+
+    def calcDistance(self, x1,x2,y1,y2):
+        if self.manhattan_dist:
+            return np.abs(x1 - x2) + np.abs(y1 - y2)
+        return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
